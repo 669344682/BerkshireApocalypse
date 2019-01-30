bcd9a6c99089a23c5a8f82aabc6254939bee3d54
raydN development <39228636+raydNDev@users.noreply.github.com>
2019-01-30 11:17:25 +0100

    Fixed a Bug.

--- BerkshireApocalypse/Client/Sonstiges/Language.lua
+++ BerkshireApocalypse/Client/Sonstiges/Language.lua
@@ -6,94 +6,52 @@
 
 ]]--
 
-language = {
-	["List"] = {"German","English"},
-	["Sprachen"] = {
-		["German"] = 0,
-		["English"] = 1},
-	[0] = {
-		[1] = {"Herzlich Willkommen auf Berkshire Apocalypse. Bei uns erwarten dich nicht nur dutzende Zombies, welche du erledigen musst, sondern auch einige gute Features, welche evtl. nicht ganz üblich für einen Zombie-Server sind, den Spielspaß aber auf jeden Fall fördern und dafür sorgen, dass es auch nach vielen Stunden noch spannend bleibt.\n\nDas Team wünscht dir viel Spaß auf Berkshire Apocalypse!","Registrieren","Einloggen","Als Gast spielen","Willkommen zurück auf Berkshire Apocalypse! Bitte fülle die unteren Felder aus und klicke anschließend auf 'Einloggen', um dich in deinen Account einzuloggen. Solltest du noch keinen Account bei uns haben, wechsel bitte in die Kategorie 'Registrieren' und lege dir einen Account an.\n\nDas Team wünscht dir viel Spaß auf Berkshire Apocalypse!"},
-		[2] = {"Einloggen","Registrieren","Sprache","Team","Events"},
-		[3] = {"Waffenladen","Team","Updates","Skinshop","Spenden"," ","Coinshop","Topliste"},
-		[4] = {"Schließen","Skin","Preis","Kaufen","Du hast keinen Skin ausgewählt!","Waffe","Munition"},
-		[5] = {"Spielerliste","Spieler","Zombiekills","Tode","Spielstunden","Level","Information","Ping"},
-		[6] = {"Hilfemenü","Klicke eine Kategorie an, um Informationen zu ihr zu erhalten.","Kategorie"},
-		[7] = {"Du hast keine Waffe ausgewählt!"},
-		[8] = {"Hier hast du die Möglichkeit, für 50.000$ ein Team zu gründen, welches dir diverse Vorteile verschafft. Was genau das für Vorteile sind, erfährst du im Hilfemenü unter F1.","Team gründen","Du hast keinen Namen angegeben!","Du hast keinen Spieler ausgewählt!","Du hast keine Summe angegeben!"},
-		[9] = {"Spieler","Rang","Status","Menge","Inviten","Uninviten","Einzahlen","Auszahlen","Lager"},
-		[10] = {"Du hast keine Sprache ausgewählt!","Sprache","Sprache ändern","Deine Sprache wurde auf "," gestellt."},
-		--[11] = {"Jährlich findet vom 01.04 - 01.05 unser Osterevent statt.","Jährlich findet vom 01.07 - 01.08 unser Sommer-Event statt.","Jährlich findet vom 31.10 - 30.11 unser Halloween-Event statt.","Jährlich findet vom 01.12 - 30.12 unser Weihnachtsevent statt.","Jährlich findet vom 31.12 - 09.01 unser Neujahrsevent statt."},
-		[12] = {"Inhaber","Mitglied"},
-		[13] = {"Sollten Sie Interesse daran haben, unser Projekt zu unterstützen, haben Sie die Möglichkeit per Paysafecard zu spenden. Als Zeichen unserer Dankbarkeit erhalten Sie pro Euro einen Coin. Mit Coins können Sie sich exklusive Artikel in unserem Coinshop kaufen. Füllen Sie bitte die unteren Felder aus klicken Sie anschließend auf 'Absenden'. Nachdem ihre Spende bearbeitet wurde, werden Ihnen die Coins gutgeschrieben. Vorausgesetzt die angegebenen Codes sind gültig.","Spenden","Absenden","Die Bearbeitung deiner Spende findet binnen 72 Stunden an.","Der angegebene Code kurz ist keine 16 Zeichen lang!","Einer der angegebenen Codes sind keine 16 Zeichen lang!"},
-		[14] = {"Du kannst nur Zahlen angeben!"},
-		[15] = {"Hier hast du die Möglichkeit, deine Coins gegen exklusive Artikel wie z.B. ein Waffenlager einzutauschen. Es gibt zwei Möglichkeiten, an Coins zu gelangen. 1) Spenden 2) Bonuspunkte, welche man durch das Töten von Zombies bekommt, gegen Coins eintauschen.","Deine Coins: ","Bonuspunkte eintauschen","Artikel","Bonuspunkte","Tauschen","Du hast keine Anzahl von Coins angegeben!","Hier hast du die Möglichkeit, deine Bonuspunkte gegen Coins zu tauschen. Ein Coin kostet 1000 Bonuspunkte. Trage in das untere Feld ein, wie viele Coins du möchtest und klicke anschließend auf 'Tauschen'.","Du hast keinen Artikel ausgewählt!"},
-		[16] = {"Bitte wähle einen Spieler aus und gib einen Grund an!","Bitte wähle einen Spieler aus und gib eine Summe an!"},
-		[17] = {"Bonuspunkt","Kiste","Medikit","Coin","Pokal","Zombieskin"},
-		[18] = {"Erfahrungspunkte","- Leertaste zum schließen -"},
-		[19] = {"Achievement erreicht!","+ 1500$ & 1 Pokal"},
-		[20] = {"Level","Zombiekills","Tode","Bonuspunkte","Coins","Pokale","Geld"},
-		[21] = {"Topliste","Anzeigen","Wähle eine Kategorie aus und dir wird eine Liste mit den 15 besten Spielern dieser angezeigt."},
-		[22] = {"Klicke auf ein Achievement, um Informationen zu diesem zu erhalten.","Erreicht"},
-		[23] = {"Deine Waffen:","Waffen im Lager:","Waffenlager","Einlagern","Auslagern","Du hast keine Waffe ausgewählt!"},},
-	[1] = {
-		[1] = {"Welcome on Berkshire Apocalypse! On this server you will find tons of zombies and some good features, which may not be quit common for a zombie server, but definitely promote the fun and make sure that you had a lot of fun.\n\nThe Berkshire Apocalypse team whishes you a lot of fun!","Register","Login","Play as a guest","Welcome back on Berkshire Apocalypse! Please fill your datas in the fields below and click on 'Login' to sign in with your account. If you don't have and account on our server, please switch to the category 'Register' to create a account.\n\nThe Berkshire Apocalypse team whishes you a lot of fun!"},
-		[2] = {"Login","Register","Language","Team","Events"},
-		[3] = {"Weapon Store","Team","Updates","Skinshop","Donate"," ","Coinshop","Toplist"},
-		[4] = {"Close","Skin","Price","Buy","You have not selected a skin!","Weapon","Ammo"},
-		[5] = {"Playerlist","Player","Zombiekills","Deaths","Playingtime","Level","Information","Ping"},
-		[6] = {"Helpmenue","Click on a category to get information about it.","Category"},
-		[7] = {"You have not selected a weapon!"},
-		[8] = {"Here you can create a team for 50.000$. A team give you a lot of benefits. More informations about the benefits of a team you wil find in our helpmenu under F1.","Create a team","You have not given a name!","You have not selected a player!","You have not specified a sum!"},
-		[9] = {"Player","Rang","Status","Amount","Invite","Uninvite","Deposit","Pay off","Depot"},
-		[10] = {"You have not selected a language!","Language","Change language","Your language has been set to ","."},
-		--[11] = {"Every year from 01.04 - 01.05 our Easter-Event takes place.","Every year from 01.07 - 01.08 our Summer-Event takes place.","Every year from 31.10 - 30.11 our Halloween-Event takes place.","Every year from 01.12 - 30.12 our Christmas-Event takes place.","Every year from 31.12 - 09.01 our New Year-Event takes place."},
-		[12] = {"Owner","Member"},
-		[13] = {"If you are interested in supporting our project, you have the opportunity to donate via Paysafecard. As a token of our gratitude, you will receive a Berkshire Coin per Euro. With Coins you can buy exclusive items in our Coinshop. Please fill in the fields below and click on 'Submit'. After your donation has been processed, the coins will be credited to you. Provided the specified codes are valid.","Donate","Submit","The processing of your donation will take place within 72 hours.","The given code must be 16 characters long!","The given codes must be 16 characters long!"},
-		[14] = {"You can only enter numbers!"},
-		[15] = {"Here you have the opportunity to exchange your coins for exklusive stuff. There are two ways to get coins. 1) Donate 2) Exchange bonuspoints, which you get by killing zombies, for coins.","Your coins: ","Exchange bonuspoints","Article","Bonuspoints","Exchange","You have not specified a number of coins!","Here you have the opportunity to exchange your bonus points for coins. A coin costs 1000 bonus points. Enter in the lower field how many coins you want and then click on 'Exchange'.","You have not selected any item!"},
-		[16] = {"Please select a player and give a reason!","Please choose a player and enter a sum!"},
-		[17] = {"Bonuspoint","Chest","Medikit","Coin","Trophy","Zombieskin"},
-		[18] = {"Experience-Points","- Space to close -"},
-		[19] = {"You reached a Achievement!","+ 1500$ and 1 trophy"},
-		[20] = {"Level","Zombiekills","Deaths","Bonuspoints","Coins","Trophys","Money"},
-		[21] = {"Toplist","Show","Select a category to see a list of the top 15 players of the category."},
-		[22] = {"Click on an achievement to get information about this.","Reached"},
-		[23] = {"Your weapons:","Weapons in your Depot:","Weapon Depot","Store","Take","You have not selected a weapon!"},},}
+peds = {
+	-- ID, x, y, z, rotation, text
+	{100,-1968.5,114.30000305176,27.700000762939,0,"waffenladen.open"}, -- Waffenladen am Bahnhof
+	{248,-1974.9,162.60000610352,27.700000762939,180,"team.create"}, -- Team gründen
+	{114,-1973.5,162.60000610352,27.700000762939,180,"updates.create"}, -- Updates
+	{299,-1972.1,162.60000610352,27.700000762939,180,"skinshop.open"}, -- Skinshop
+	{181,-1970.7,162.60000610352,27.700000762939,180,"spenden.window"}, -- Spenden
+	{141,-1962.1999511719,156.69999694824,27.700000762939,90,"coinshop.open"}, -- Coinshop
+	{26,-1962.1999511719,155.29999694824,27.700000762939,90,"Topliste.window"},} -- Topliste
+	
+function peds.load()
+	for _,v in ipairs(peds)do
+		peds.ped = createPed(v[1],v[2],v[3],v[4],v[5]);
+		setElementFrozen(peds.ped,true);
+		setElementData(peds.ped,"event",v[6]);
 		
-function language.change()
-	if(isElement(GUIEditor.window[1]))then destroyElement(GUIEditor.window[1])end
-	
-    GUIEditor.window[1] = guiCreateWindow(0.30, 0.36, 0.24, 0.30, language[language.get()][10][2], true)
-
-    GUIEditor.gridlist[1] = guiCreateGridList(0.03, 0.10, 0.94, 0.72, true, GUIEditor.window[1])
-    sprache = guiGridListAddColumn(GUIEditor.gridlist[1]," ", 0.9)
-
-    GUIEditor.button[1] = guiCreateButton(0.03, 0.86, 0.94, 0.11, language[language.get()][10][3], true, GUIEditor.window[1])
-	
-	GUIEditor.set("set");
-	
-	for key,_ in pairs(language["List"])do
-		row = guiGridListAddRow(GUIEditor.gridlist[1])
-		guiGridListSetItemText(GUIEditor.gridlist[1],row,sprache,language["List"][key],false,false)
+		addEventHandler("onClientPedDamage",peds.ped,function()
+			cancelEvent();
+		end)
 	end
 	
-	addEventHandler("onClientGUIClick",GUIEditor.button[1],function()
-		local sprache = guiGridListGetItemText(GUIEditor.gridlist[1],guiGridListGetSelectedItem(GUIEditor.gridlist[1]),1)
-					
-		if(not(sprache == ""))then
-			setElementData(localPlayer,"language",language["Sprachen"][sprache]);
-			infobox.create(language[language.get()][10][4]..sprache..language[language.get()][10][5],"info");
-			
-			guiSetText(GUIEditor.window[1],language[language.get()][10][2]);
-			guiSetText(GUIEditor.button[1],language[language.get()][10][3]);
-			
-			for i = 1,5 do
-				anmeldebereich.texts[i] = language[language.get()][2][i];
+	addEventHandler("onClientRender",root,function()
+		if(getElementData(localPlayer,"loggedin") == 1)then
+			for k,v in ipairs(peds)do
+				px,py,pz = v[2],v[3],v[4]
+				if(getDistanceBetweenPoints3D(px,py,pz,getElementPosition(localPlayer)) <= 10)then
+					sx,sy = getScreenFromWorldPosition(px,py,pz+1.1,1000,true)
+					if(sx and sy)then
+						if(GUIEditor.isWindow())then
+							dxDrawText(language[language.get()][3][k],sx,sy,sx,sy+25,tocolor(255,255,255),1,"default-bold","center","bottom",false,false,false,true);
+						end
+					end
+				end
 			end
-		else infobox.create(language[language.get()][10][1],"error")end
-	end,false)
+		end
+	end)
 end
+peds.load()
 
-function language.get()
-	return getElementData(localPlayer,"language");
-end
+addEventHandler("onClientClick",root,function(button,state,absx,absy,wx,wy,wz,element)
+	if(element and getElementType(element) == "ped" and state == "down")then
+		local x,y,z = getElementPosition(localPlayer);
+		if(getDistanceBetweenPoints3D(x,y,z,wx,wy,wz) <= 3)then
+			if(getElementData(element,"event"))then
+				triggerEvent(getElementData(element,"event"),localPlayer);
+			end
+		end
+	end
+end)
