0fccfbaf531ec24e8ee200afa9ee43120a33463f
raydN development <39228636+raydNDev@users.noreply.github.com>
2019-01-30 12:51:55 +0100

    Update Infobox.lua

--- BerkshireApocalypse/Client/Anzeigen/Infobox.lua
+++ BerkshireApocalypse/Client/Anzeigen/Infobox.lua
@@ -18,12 +18,13 @@ function infobox.create(text,typ)
 	infobox.g = infobox["Infos"][typ][2];
 	infobox.b = infobox["Infos"][typ][3];
 	infobox.picture = infobox["Infos"][typ][4];
+	if(isTimer(infobox.timer))then killTimer(infobox.timer)end
 	
 	if(infobox.state == false)then
 		infobox.state = true;
 		addEventHandler("onClientRender",root,infobox.render);
 		
-		setTimer(function()
+		infobox.timer = setTimer(function()
 			infobox.state = false;
 			removeEventHandler("onClientRender",root,infobox.render);
 		end,8000,1)
@@ -33,7 +34,13 @@ addEvent("infobox.create",true)
 addEventHandler("infobox.create",root,infobox.create)
 
 function infobox.render()
-    dxDrawRectangle(1033*(x/1440), 791*(y/900), 397*(x/1440), 99*(y/900), tocolor(0, 0, 0, 225), false)
-    dxDrawText(infobox.text,1142*(x/1440), 801*(y/900), 1420*(x/1440), 880*(y/900), tocolor(infobox.r,infobox.g,infobox.b, 255), 1.00, "default-bold", "center", "center", false, true, false, false, false)
-    dxDrawImage(1043*(x/1440), 801*(y/900), 89*(x/1440), 79*(y/900), infobox.picture, 0, 0, 0, tocolor(255, 255, 255, 255), false)
+	if(GUIEditor.isWindow())then
+		dxDrawRectangle(1033*(x/1440), 791*(y/900), 397*(x/1440), 99*(y/900), tocolor(0, 0, 0, 225), true)
+		dxDrawText(infobox.text,1142*(x/1440), 801*(y/900), 1420*(x/1440), 880*(y/900), tocolor(infobox.r,infobox.g,infobox.b, 255), 1.00, "default-bold", "center", "center", false, true, true, false, false)
+		dxDrawImage(1043*(x/1440), 801*(y/900), 89*(x/1440), 79*(y/900), infobox.picture, 0, 0, 0, tocolor(255, 255, 255, 255), true)
+	else
+		dxDrawRectangle(1033*(x/1440), 741*(y/900), 397*(x/1440), 99*(y/900), tocolor(0, 0, 0, 225), true)
+		dxDrawText(infobox.text,1142*(x/1440), 706*(y/900), 1420*(x/1440), 880*(y/900), tocolor(infobox.r,infobox.g,infobox.b, 255), 1.00, "default-bold", "center", "center", false, true, true, false, false)
+		dxDrawImage(1043*(x/1440), 751*(y/900), 89*(x/1440), 79*(y/900), infobox.picture, 0, 0, 0, tocolor(255, 255, 255, 255), true)
+	end
 end
